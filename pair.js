d0ddcc06df094b4f9f9749c05db9250bbdc259ac
next55x <deploytech597@gmail.com>
2025-04-17 11:30:01 +0300

    Update pair.js

--- pair.js
+++ pair.js
@@ -57,17 +57,17 @@ router.get('/', async (req, res) => {
                let session = await Pair_Code_By_Fredie_Tech.sendMessage(Pair_Code_By_Fredie_Tech.user.id, { text: 'LUCKY-MD;;;=>' + b64data });
 
                let FREDI_TECH_TEXT = `
-✧LUCKY MD DEVICE SUCCESSFUL CONNECTED✧
+✧TIMNASA MD DEVICE SUCCESSFUL CONNECTED✧
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-🤦  *Creator* ☞ ✧FREDI EZRA✧
-🍽️  *Repo*  ☞ https://github.com/Fred1e/LUCKY_MD
+🤦  *Creator* ☞ ✧Timnasa.timothy✧
+🍽️  *Repo*  ☞ https://github.com/Next5x/TIMNASA_TMD1
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-🫂  WhattsApp Channel ☞ https://whatsapp.com/channel/0029VaihcQv84Om8LP59fO3f
+🫂  WhattsApp Channel ☞ https://whatsapp.com/channel/0029VajweHxKQuJP6qnjLM31
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-🥂 *Contact Owne* ☞ https://wa.me/255752593977
-FREDIETECH
+🥂 *Contact Owne* ☞ https://wa.me/255784766591
+TIMNASA-TECH
 ▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-> ©*FREDIE TECH 2025 SCRIPT*
+> ©*TIMNASA TECH 2025 SCRIPT*
 
 _Don't Forget To Give Star To My Repo_`
  await Pair_Code_By_Fredie_Tech.sendMessage(Pair_Code_By_Fredie_Tech.user.id,{text:FREDI_TECH_TEXT},{quoted:session})
@@ -78,7 +78,7 @@ _Don't Forget To Give Star To My Repo_`
         return await removeFile('./temp/'+id);
             } else if (connection === "close" && lastDisconnect && lastDisconnect.error && lastDisconnect.error.output.statusCode != 401) {
                     await delay(10000);
-                    FREDI_TECH_PAIR_CODE();
+                    TIMNASA_TECH_PAIR_CODE();
                 }
             });
         } catch (err) {
@@ -89,6 +89,6 @@ _Don't Forget To Give Star To My Repo_`
          }
         }
     }
-    return await FREDI_TECH_PAIR_CODE()
+    return await TIMNASA_TECH_PAIR_CODE()
 });
 module.exports = router
